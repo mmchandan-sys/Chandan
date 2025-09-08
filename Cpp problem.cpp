3294f59222d892a8c966f12409a196551032bedf
Chandan M M <chandanmm506@gmail.com>
2025-09-08 11:12:36 +0530

    Create Cpp problem.cpp

---
+++ Cpp problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << num << " is Even\n";
+    else
+        cout << num << " is Odd\n";
+
+    return 0;
+}
