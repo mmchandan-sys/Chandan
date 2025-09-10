18d6e118d2b95c8cae627c82c6261ae2e7ca0093
Chandan M M <chandanmm506@gmail.com>
2025-09-10 10:06:55 +0530

    Update Cpp problem.cpp

--- Cpp problem.cpp
+++ Cpp problem.cpp
@@ -13,3 +13,40 @@ int main() {
 
     return 0;
 }
+
+
+#👨‍🏫Student Grade Calculator
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of subjects: ";
+    cin >> n;
+
+    float marks[n], sum = 0;
+    for (int i = 0; i < n; i++) {
+        cout << "Enter marks of subject " << i + 1 << ": ";
+        cin >> marks[i];
+        sum += marks[i];
+    }
+
+    float avg = sum / n;
+    cout << "\nAverage Marks = " << avg << endl;
+
+    if (avg >= 90)
+        cout << "Grade: A+" << endl;
+    else if (avg >= 80)
+        cout << "Grade: A" << endl;
+    else if (avg >= 70)
+        cout << "Grade: B" << endl;
+    else if (avg >= 60)
+        cout << "Grade: C" << endl;
+    else if (avg >= 50)
+        cout << "Grade: D" << endl;
+    else
+        cout << "Grade: F" << endl;
+
+    return 0;
+}
